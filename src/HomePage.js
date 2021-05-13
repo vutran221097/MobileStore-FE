63919fae9eb541557369e193f0111a7827bf857d
tranvu221097@gmail.com <tranvu221097@gmail.com>
2021-05-13 14:25:57 +0700

    add FE

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import './HomePage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar.js'
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <Navbar />
+    </div>
+  );
+}
+
+export default HomePage;
